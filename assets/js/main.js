a4423a5303849b20c1218ad86620db7c0bcc71d8
Tatyana404 <kot.kate2020@gmail.com>
2024-06-09 01:13:11 +0200

    refactor

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,19 @@
-let timerId;
-
 const startButton = document.getElementById("start");
 const stopButton = document.getElementById("stop");
 const clock = document.getElementById("clock");
 
+let timerId;
+
+const formatTimeUnit = (unit) => (unit < 10 ? `0${unit}` : unit);
+
 const updateClock = () => {
   const now = new Date();
+
   const hours = now.getHours();
   const minutes = now.getMinutes();
   const seconds = now.getSeconds();
 
-  clock.textContent = `${hours}:${minutes}:${seconds}`;
+  clock.textContent = `${formatTimeUnit(hours)}:${formatTimeUnit(minutes)}:${formatTimeUnit(seconds)}`;
 };
 
 startButton.addEventListener("click", () => {
